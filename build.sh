2905bc7cdb28284ea7128d6494f5bd401057c3ff
bluesky <chenchongbiao@deepin.org>
2023-09-26 18:07:33 +0800

    chore: update workflows

--- build.sh
+++ build.sh
@@ -12,7 +12,7 @@ mkdir -p /beige-rootfs/etc/apt/trusted.gpg.d
 
 cp deepin.gpg /beige-rootfs/etc/apt/trusted.gpg.d
 
-multistrap -f beige.multistrap
+multistrap -f beige-${1}.multistrap
 
 cp sources.list /beige-rootfs/etc/apt/sources.list
 
