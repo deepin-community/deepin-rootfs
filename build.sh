eedc536e6fe361991877f8c1fe5045634180e34d
bluesky <379865549@qq.com>
2023-10-08 23:22:46 +0800

    fix: fix build rootfs
    使用 docker 构建根文件系统在 wsl 打包程序会错误，进行修复

--- build.sh
+++ build.sh
@@ -7,11 +7,10 @@ apt update
 # 不进行交互安装
 export DEBIAN_FRONTEND=noninteractive
 
-# 稳定源里暂时没有包，暂时手动下载包安装
-apt install -y wget && \
-    wget http://ftp.kr.debian.org/debian/pool/main/libp/libparse-debian-packages-perl/libparse-debian-packages-perl_0.03-5_all.deb && \
-    wget http://ftp.kr.debian.org/debian/pool/main/m/multistrap/multistrap_2.2.11_all.deb && \
-    apt install -y ./*deb
+apt install multistrap -y
+
+mkdir -p /beige-rootfs/etc/apt/trusted.gpg.d
+cp deepin.gpg /beige-rootfs/etc/apt/trusted.gpg.d
 
 arch=${1}
 echo -e "[General]\n\
@@ -27,7 +26,6 @@ bootstrap=Deepin\n\
 [Deepin]\n\
 packages=apt ca-certificates locales-all sudo systemd\n\
 source=https://community-packages.deepin.com/beige/\n\
-keyring=deepin-keyring\n\
 suite=beige\n\
 " >/beige.multistrap
 
